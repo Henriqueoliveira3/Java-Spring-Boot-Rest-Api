401890628f218d7f26900ccbec1348ee26654768
Henriqueoliveira3 <67375942+Henriqueoliveira3@users.noreply.github.com>
2023-08-05 13:36:15 -0300

    Adição de Validações
    Adição de Validações com Bean Validations.

--- api/src/main/java/med/voll/api/medico/DadosCadastrosMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastrosMedico.java
@@ -1,7 +1,25 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.enderecos.DadosEndereco;
 
-public record DadosCadastrosMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastrosMedico(
+		@NotBlank
+		String nome,
+		@NotBlank
+		@Email
+		String email,
+		@NotBlank
+		@Pattern(regexp = "\\d{4,6}")
+		String crm,
+		@NotNull
+		Especialidade especialidade, 
+		@NotNull
+		@Valid
+		DadosEndereco endereco) {
 	
 }
