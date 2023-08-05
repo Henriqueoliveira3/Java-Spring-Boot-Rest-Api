51b69cb514027b0d639904a13ba57268dc463438
Henriqueoliveira3 <67375942+Henriqueoliveira3@users.noreply.github.com>
2023-08-05 17:05:51 -0300

    Adicionado paginação.

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,7 +1,8 @@
 package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,8 +31,8 @@ public class MedicoController {
 	}
 	
 	@GetMapping
-	public List<DadosListagemMedico> listar(){
-		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+	public Page<DadosListagemMedico> listar(Pageable paginacao){
+		return repository.findAll(paginacao).map(DadosListagemMedico::new);
 	}
 	
 }
