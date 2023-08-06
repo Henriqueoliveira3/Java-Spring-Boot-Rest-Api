ba6228634979ad10841c9d6f434ffd85588032da
Henriqueoliveira3 <67375942+Henriqueoliveira3@users.noreply.github.com>
2023-08-06 19:47:43 -0300

    Padronizando retornos da API
    Padronizado: GET, PUT, DELETE.

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosAtualizarMedico;
 import med.voll.api.medico.DadosCadastrosMedico;
+import med.voll.api.medico.DadosDetalhamentoMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -35,22 +37,27 @@ public class MedicoController {
 	}
 	
 	@GetMapping
-	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
-		return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+	public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+		var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+		return ResponseEntity.ok(page);
 	}
 	
 	@PutMapping
 	@Transactional
-	public void atualizar(@RequestBody @Valid DadosAtualizarMedico dados) {
+	public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizarMedico dados) {
 		var medico = repository.getReferenceById(dados.id());
 		medico.atualizarInformacoes(dados);
+		
+		return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
 	}
 	
 	@DeleteMapping("/{id}")
 	@Transactional
-	public void excluir(@PathVariable Long id) {
+	public ResponseEntity excluir(@PathVariable Long id) {
 		var medico = repository.getReferenceById(id);
 		medico.desativarMedico();
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 }
